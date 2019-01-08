cfc8218b7001733cc78920b74fc7b9975101691a
Jeff Rowberg <jeff@rowberg.net>
2019-01-07 21:05:15 -0500

    Move protocol methods into class methods

--- generic_ltv.py
+++ generic_ltv.py
@@ -2,7 +2,8 @@ import perilib
 
 class LTVProtocol(perilib.protocol.stream.core.StreamProtocol):
 
-    def test_packet_complete(self, buffer, is_tx=False):
+    @classmethod
+    def test_packet_complete(cls, buffer, is_tx=False):
         # simple terminal condition for LTV data, where L/T are single bytes
         # [length] [type] [v0, v1, ..., v<length>]
         if len(buffer) > 0 and len(buffer) == buffer[0] + 1:
@@ -10,7 +11,8 @@ class LTVProtocol(perilib.protocol.stream.core.StreamProtocol):
         else:
             return perilib.protocol.stream.core.ParserGenerator.STATUS_IN_PROGRESS
 
-    def get_packet_from_buffer(self, buffer, port_info=None, is_tx=False):
+    @classmethod
+    def get_packet_from_buffer(cls, buffer, parser_generator=None, is_tx=False):
         definition = {
             "name": "ltv_packet",
             "args": [
@@ -19,7 +21,7 @@ class LTVProtocol(perilib.protocol.stream.core.StreamProtocol):
                 { "name": "value", "type": "uint8a-greedy" }
             ]
         }
-        return LTVPacket(buffer=buffer, definition=definition, port_info=port_info)
+        return LTVPacket(buffer=buffer, definition=definition, parser_generator=parser_generator)
 
 class LTVPacket(perilib.protocol.stream.core.StreamPacket):
 
--- generic_tlv.py
+++ generic_tlv.py
@@ -2,7 +2,8 @@ import perilib
 
 class TLVProtocol(perilib.protocol.stream.core.StreamProtocol):
 
-    def test_packet_complete(self, buffer, is_tx=False):
+    @classmethod
+    def test_packet_complete(cls, buffer, is_tx=False):
         # simple terminal condition for TLV data, where T/L are single bytes
         # [type] [length] [v0, v1, ..., v<length>]
         if len(buffer) > 1 and len(buffer) == buffer[1] + 2:
@@ -10,7 +11,8 @@ class TLVProtocol(perilib.protocol.stream.core.StreamProtocol):
         else:
             return perilib.protocol.stream.core.ParserGenerator.STATUS_IN_PROGRESS
 
-    def get_packet_from_buffer(self, buffer, port_info=None, is_tx=False):
+    @classmethod
+    def get_packet_from_buffer(cls, buffer, parser_generator=None, is_tx=False):
         definition = {
             "name": "tlv_packet",
             "args": [
@@ -19,7 +21,7 @@ class TLVProtocol(perilib.protocol.stream.core.StreamProtocol):
                 { "name": "value", "type": "uint8a-greedy" }
             ]
         }
-        return TLVPacket(buffer=buffer, definition=definition, port_info=port_info)
+        return TLVPacket(buffer=buffer, definition=definition, parser_generator=parser_generator)
 
 class TLVPacket(perilib.protocol.stream.core.StreamPacket):
 
