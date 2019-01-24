21dbf8c59455234c1550f12d7ad0c6a05c02cfaf
Jeff Rowberg <jeff@rowberg.net>
2019-01-23 21:34:43 -0500

    Add documentation to module init files

--- __init__.py
+++ __init__.py
@@ -1,3 +1,8 @@
+"""
+This module provides protocol and packet definitions for commonly used methods
+of communication including type-length-value (TLV) and length-type-value (LTV).
+"""
+
 # .py files
 from .generic_tlv import *
 from .generic_ltv import *
