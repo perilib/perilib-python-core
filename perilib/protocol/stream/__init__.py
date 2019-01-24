334512fdd333481da3b71aadc45ae6a92d8ed0e6
Jeff Rowberg <jeff@rowberg.net>
2019-01-23 21:35:31 -0500

    Add documentation to module init files

--- perilib/protocol/stream/__init__.py
+++ perilib/protocol/stream/__init__.py
@@ -1,2 +1,10 @@
+"""
+This module provides protocol, packet, and parser/generator implementations for
+generic stream-based communication. Some protocols and devices will be able to
+use much of this code as-is, although at least the protocol definition will need
+to be subclassed to provide boundary and vocabulary definitions. Other protocols
+and devices may need to customize significant chunks of the implementation.
+"""
+
 # .py files
 from .core import *
