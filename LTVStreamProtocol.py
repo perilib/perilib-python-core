eafba758885c207cefcf6fb8bfef0b69746653ee
Jeff Rowberg <jeff@rowberg.net>
2019-04-13 22:52:45 -0400

    Update protocol definitions to use new_byte argument for boundary tests

--- LTVStreamProtocol.py
+++ LTVStreamProtocol.py
@@ -3,10 +3,10 @@ import perilib
 class LTVStreamProtocol(perilib.StreamProtocol):
 
     @classmethod
-    def test_packet_complete(cls, buffer, is_tx=False):
+    def test_packet_complete(cls, buffer, new_byte, is_tx=False):
         # simple terminal condition for LTV data, where L/T are single bytes
         # [length] [type] [v0, v1, ..., v<length>]
-        if len(buffer) > 0 and len(buffer) == buffer[0] + 1:
+        if len(buffer) > 0 and len(buffer) + 1 == buffer[0] + 1:
             return perilib.ParseStatus.COMPLETE
         else:
             return perilib.ParseStatus.IN_PROGRESS
